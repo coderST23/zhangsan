2bb69cade58d64765530026c287d781aaab22c0a
stenxt <s229189410@163.com>
2020-06-22 12:22:01 +0800

    第一次向GitHub提交代码

---
+++ deo01.py
@@ -0,0 +1,44 @@
+# # '''
+# # print("hello world!") #字符串
+# # print(2333) #整数
+# # print(2.333) #小数
+# # print(True) #布尔值
+# # print(( )) 
+# # print([ ])
+# # print({ }) 
+
+# # 锄禾日当午
+# # 汗滴禾下土
+
+# # print(100-10/2*(6+5))
+# # print ("嘻嘻"*10)
+# # '''
+# # '''
+# # a = input('请输入：')
+# # b = input('请输入：')
+# # print('input获取的内容；', a+b)
+
+# # a = input()
+# # print ('数学',a)
+# # '''
+
+# # # print(type('书序'))
+# # # print(type(223))
+# # # print(type(2.33))
+# # # print(type(True))
+# # # print(type(()))
+# # # print(type([]))
+# # # print(ty({}))
+
+
+# # a =float (input('请输入：'))
+# # b =float (input('请输入：'))
+# # print('input获取的内容；', a/b)
+
+# a = 'hao   '
+# print(len(a))
+
+
+a = input('1请输入：')
+b = input('2请输入：')
+print(len(a+b))
